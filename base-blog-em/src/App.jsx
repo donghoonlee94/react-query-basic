506eb77f01f57da6421b395542f895a981d901d3
동훈 이 <ethan@kyobobook.com>
2022-11-14 11:06:42 +0900

    test

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,10 +1,12 @@
-import { Posts } from './Posts';
-import './App.css';
+import { Posts } from "./Posts";
+import "./App.css";
 
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
+
+console.log("test");
 
 function App() {
   return (
