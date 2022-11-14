252971f1f9b5fbbcce4c91a96d3e691f7b0eb305
동훈 이 <ethan@kyobobook.com>
2022-11-14 14:49:38 +0900

    add: spacies infinite scroll

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
 import { Species } from "./Species";
 
 const initialUrl = "https://swapi.dev/api/species/";
@@ -8,6 +9,35 @@ const fetchUrl = async (url) => {
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isError,
+    error,
+    isLoading,
+    isFetching,
+  } = useInfiniteQuery(
+    "sw-species",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+
+  if (isLoading) return <div className="loading">...loading</div>;
+  if (isError) return <div>{error.toString()}</div>;
+
+  return (
+    <>
+      {isFetching && <div className="loading">...loading</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((specie) => {
+            return <Species key={specie.name} {...specie} />;
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
